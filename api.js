402cb2a0b81a9894e28ebafb4f976097453797bb
Konstantinos Oikonomou <kons.oikonomou@gmail.com>
2019-03-23 18:03:21 -0500

    add read api functionality

---
+++ api.js
@@ -0,0 +1,15 @@
+const SPREADSHEET_ID = '1uS4Nr_Vl6ZljcKsmLRnDS-X9uYIlNvkS4lQAhcSXhIg';
+const KEY = 'AIzaSyB5NPQXUnbJYcNBwZ0vHwbpkCv8Jp-EB5Y';
+
+function build_request() {
+    let BASE_URL = `https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_ID}/values/Sheet1!A1:D7?key=${KEY}`;
+    return BASE_URL; 
+}
+
+fetch(build_request()).then(resp => {
+    return resp.json();
+}).then( data => {
+    console.log(data);
+}).catch(err => {
+    console.log(err);
+});
