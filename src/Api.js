e0471e2fc0e1e20fb908775341b9b00e433dee96
cktang88 <arctangent20@yahoo.com>
2019-03-24 02:51:18 -0500

    Fix fetch comments

--- src/Api.js
+++ src/Api.js
@@ -1,38 +1,45 @@
-const SPREADSHEET_ID = '1uS4Nr_Vl6ZljcKsmLRnDS-X9uYIlNvkS4lQAhcSXhIg';
+const SPREADSHEET_ID = 
+//'1b3cMRIILqPjdNPxTmAhjKAw0oYgXV1D8Aro2lrTvdDQ'
+'1uS4Nr_Vl6ZljcKsmLRnDS-X9uYIlNvkS4lQAhcSXhIg';
 const KEY = 'AIzaSyB5NPQXUnbJYcNBwZ0vHwbpkCv8Jp-EB5Y';
 
 const makeURL = range => `https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_ID}/values/${range}?key=${KEY}`;
 
-const commentsURL = 'https://api.myjson.com/bins/crimsongoogform';
+const commentsURL = 'https://api.myjson.com/bins/95ny6';
 
 const fetchData = () => {
     return fetch(makeURL('Sheet1')).then(resp => resp.json())
-}
+};
 
 // fetch comments by key
 const fetchComments = (key) => {
-    return fetch(makeURL)
+    console.log(key)
+    return fetch(commentsURL)
         .then(data => data.json())
-        .then(items => items[key]);
 };
 
-const postComment = (key, author, text) => {
+const postComment = (key, text, author) => {
 
     const newcomment = {
-        timestamp: Date.now(),
+        timestamp: (new Date()).toLocaleString(),
         author,
         text
     };
-    // fetch, and then update
-    fetchComments(key)
-        .then(
-        entry => putHelper(BASE_URL, (entry ? entry.push(newcomment) : [newcomment]))
-        )
-        .then(data => console.log(JSON.stringify(data))) // JSON-string from `response.json()` call
-        .catch(error => console.error(error));
-}
 
-const putHelper = (url = commentsURL, data = {}) => {
+    // fetch, and then update
+    return fetchComments(key)
+        .then(data => {
+            if (!data[key])
+                data[key] = [];
+            data[key].push(newcomment);
+            return data;
+        })
+        .then(data => putHelper(commentsURL, data))
+        .then(data => console.log(data)) // JSON-string from `response.json()` call
+        .catch(error => console.error(error));
+};
+
+const putHelper = (url, data) => {
     // Default options are marked with *
     return fetch(url, {
             method: "PUT", // *GET, POST, PUT, DELETE, etc.
@@ -48,9 +55,10 @@ const putHelper = (url = commentsURL, data = {}) => {
             body: JSON.stringify(data), // body data type must match "Content-Type" header
         })
         .then(response => response.json()); // parses JSON response into native Javascript objects 
-}
+};
 
-export {
+module.exports = {
+    fetchComments,
     fetchData,
     postComment
 };
